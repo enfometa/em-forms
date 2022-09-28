21c4d9f9764d8aa7b84066e4ba14cf524d35d931
Hammad Ali <hammad.se@live.com>
2022-09-28 17:38:15 +0500

    Finalized webpack.config.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-
 module.exports = {
   entry: path.join(__dirname, "index.js"),
   mode: "production",
@@ -23,34 +20,9 @@ module.exports = {
           },
         },
       },
-      // {
-      //   test: /\.css$/i,
-      //   use: ["style-loader", "css-loader"],
-      // },
     ],
   },
   externals: {
     react: "react",
   },
-  //plugins: [
-  // new HtmlWebpackPlugin({
-  //   template: path.join(__dirname, "src", "index.html"),
-  // }),
-  // new CopyPlugin({
-  //   patterns: [{ from: "src/assets", to: "assets" }],
-  // }),
-  //],
-  // resolve: {
-  //   alias: {
-  //     src: path.resolve(__dirname, "src"),
-  //     components: path.resolve(__dirname, "src/components"),
-  //     hooks: path.resolve(__dirname, "src/hooks"),
-  //   },
-  // },
-  // devServer: {
-  //   // contentBase: path.join(__dirname, 'dist'),
-  //   // compress: true,
-  //   // port: 9000,
-  //   historyApiFallback: true,
-  // },
 };
