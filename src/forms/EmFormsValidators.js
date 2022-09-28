596687a262f197cd5a4d59a444652685906bc818
Hammad Ali <hammad.se@live.com>
2022-09-28 23:07:50 +0500

    formsGroup -> emForms

--- src/forms/EmFormsValidators.js
+++ src/forms/EmFormsValidators.js
@@ -1,23 +1,18 @@
-function required(form, formsGroup, param) {
+function required(form, emForms, param) {
   let isValid = true;
   let initValue = null;
   if (param !== undefined) {
     initValue = param.initialValue;
   }
-  if (
-    form.value === undefined ||
-    form.value === '' ||
-    form.value === null ||
-    (initValue !== null && initValue === form.value)
-  ) {
+  if (form.value === undefined || form.value === "" || form.value === null || (initValue !== null && initValue === form.value)) {
     isValid = false;
   }
   return isValid;
 }
 
-function maxLength(form, formsGroup, param) {
+function maxLength(form, emForms, param) {
   let isValid = true;
-  if (form.value !== undefined && form.value !== '' && form.value !== null) {
+  if (form.value !== undefined && form.value !== "" && form.value !== null) {
     if (param !== undefined && param.maxLength !== undefined) {
       let valLength = form.value.toString().length;
       if (valLength > param.maxLength) {
@@ -28,9 +23,9 @@ function maxLength(form, formsGroup, param) {
   return isValid;
 }
 
-function minLength(form, formsGroup, param) {
+function minLength(form, emForms, param) {
   let isValid = true;
-  if (form.value !== undefined && form.value !== '' && form.value !== null) {
+  if (form.value !== undefined && form.value !== "" && form.value !== null) {
     if (param !== undefined && param.minLength !== undefined) {
       let valLength = form.value.toString().length;
       if (valLength < param.minLength) {
@@ -41,11 +36,11 @@ function minLength(form, formsGroup, param) {
   return isValid;
 }
 
-function pattern(form, formsGroup, param) {
+function pattern(form, emForms, param) {
   let isValid = true;
-  if (form.value !== undefined && form.value !== '' && form.value !== null) {
+  if (form.value !== undefined && form.value !== "" && form.value !== null) {
     if (param !== undefined && param.pattern !== undefined) {
-      let regEx = new RegExp(param.pattern, 'g');
+      let regEx = new RegExp(param.pattern, "g");
       let value = form.value.toString();
       isValid = regEx.test(value);
     }
@@ -53,28 +48,28 @@ function pattern(form, formsGroup, param) {
   return isValid;
 }
 
-function email(form, formsGroup, param) {
+function email(form, emForms, param) {
   let isValid = true;
-  isValid = pattern(form, formsGroup, {pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/});
+  isValid = pattern(form, emForms, { pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ });
   return isValid;
 }
 
-function requiredIf(form, formsGroup, param) {
+function requiredIf(form, emForms, param) {
   let isValid = true;
-  let formCompareValue = formsGroup.getFormValue(param.name);
+  let formCompareValue = emForms.getFormValue(param.name);
 
   if (formCompareValue == param.value) {
-    if (form.value === undefined || form.value === null || form.value === '') {
+    if (form.value === undefined || form.value === null || form.value === "") {
       isValid = false;
     }
   }
   return isValid;
 }
 
-function compare(form, formsGroup, param) {
+function compare(form, emForms, param) {
   let isValid = true;
-  let formCompareValue = formsGroup.getFormValue(param.compareTo);
-  if (!(form.value === undefined || form.value === null || form.value === '')) {
+  let formCompareValue = emForms.getFormValue(param.compareTo);
+  if (!(form.value === undefined || form.value === null || form.value === "")) {
     if (formCompareValue !== form.value) {
       isValid = false;
     }
@@ -82,4 +77,4 @@ function compare(form, formsGroup, param) {
   return isValid;
 }
 
-export {required, maxLength, minLength, pattern, email, requiredIf, compare};
+export { required, maxLength, minLength, pattern, email, requiredIf, compare };
